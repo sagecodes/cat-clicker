19159a97b735a47bfd27503710feb76bfedaafda
Sage Elliott <wildtroll@gmail.com>
2016-08-22 22:06:52 -0700

    feat: Add second cat and change names when page is loaded

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
+var catOne = "Tim";
+var catTwo = "Tom";
 var clicks = 0;
+
+
     function onClick() {
         clicks += 1;
         document.getElementById("clicks").innerHTML = clicks;
     };
+    function catNames() {
+        document.getElementById("cat-name-one").innerHTML = catOne;
+        document.getElementById("cat-name-two").innerHTML = catTwo;
+    };
 
 
-console.log("loaded");
 
 
