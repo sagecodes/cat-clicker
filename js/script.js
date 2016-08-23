7fc8e59ec0112759894d2cf11e094dcb28f51559
Sage Elliott <wildtroll@gmail.com>
2016-08-22 22:11:59 -0700

    Feat: Add basic functionality for both cat clickers

--- js/script.js
+++ js/script.js
@@ -1,12 +1,20 @@
 var catOne = "Tim";
 var catTwo = "Tom";
-var clicks = 0;
+var clicksOne = 0;
+var clicksTwo = 0;
 
 
-    function onClick() {
-        clicks += 1;
-        document.getElementById("clicks").innerHTML = clicks;
+    function catOneCount() {
+        clicksOne += 1;
+        document.getElementById("clicks-one").innerHTML = clicksOne;
     };
+
+    function catTwoCount() {
+        clicksTwo += 1;
+        document.getElementById("clicks-two").innerHTML = clicksTwo;
+    };
+
+// Add the cat names when page is loaded
     function catNames() {
         document.getElementById("cat-name-one").innerHTML = catOne;
         document.getElementById("cat-name-two").innerHTML = catTwo;
